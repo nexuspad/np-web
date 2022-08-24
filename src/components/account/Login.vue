aa24b1e9e8cb0f26c329855235a888eade0674f4
ren <ren@nexuspad.com>
2022-08-24 11:54:56 -0400

    updated for TM

--- src/components/account/Login.vue
+++ src/components/account/Login.vue
@@ -1,24 +1,48 @@
 <template>
   <div class="np-module-container np-slim-box">
     <message :location="'TOP_STICKY'" />
-    <h2>{{npContent('log in')}} or 
-      <router-link to="/register">{{npContent('create a free account')}}</router-link>
-    </h2>
+    <h2>{{ npContent("log in") }}</h2>
     <form v-if="user === null || !user.sessionId">
       <div class="form-group">
-        <input type="text" class="form-control" :placeholder="npContent('email or username')" autocomplete="username" v-model="username">
+        <input
+          type="text"
+          class="form-control"
+          :placeholder="npContent('email or username')"
+          autocomplete="username"
+          v-model="username"
+        />
       </div>
       <div class="form-group">
-        <input type="password" class="form-control" :placeholder="npContent('password')" autocomplete="current-password" v-model="password">
-        <p style="margin:1em;"><a href="javascript:" @click="$router.push({name: 'reqpasswordreset'})">{{npContent('reset password')}}</a></p>
+        <input
+          type="password"
+          class="form-control"
+          :placeholder="npContent('password')"
+          autocomplete="current-password"
+          v-model="password"
+        />
+        <p style="margin: 1em">
+          <a
+            href="javascript:"
+            @click="$router.push({ name: 'reqpasswordreset' })"
+            >{{ npContent("reset password") }}</a
+          >
+        </p>
       </div>
       <div class="form-group">
-        <button class="btn btn-primary" v-on:click="login($event, username, password)" :disabled="posting">{{npContent('login')}}</button>
+        <button
+          class="btn btn-primary"
+          v-on:click="login($event, username, password)"
+          :disabled="posting"
+        >
+          {{ npContent("login") }}
+        </button>
       </div>
     </form>
     <b-alert :show="showLegacyAccountLink">
       You might have an older account which can be accessed at:
-      <a href="https://legacy.nexuspad.com/account.htm#/login">https://legacy.nexuspad.com/account.htm#/login</a>.
+      <a href="https://legacy.nexuspad.com/account.htm#/login"
+        >https://legacy.nexuspad.com/account.htm#/login</a
+      >.
     </b-alert>
     <div v-if="user !== null && user.sessionId">
       {{ user.sessionId }}
@@ -31,31 +55,32 @@
 </template>
 
 <script>
-import CookieLaw from 'vue-cookie-law';
-import AccountActionProvider from '../account/AccountActionProvider';
-import AccountService from '../../core/service/AccountService';
-import Message from '../common/Message';
-import EventManager from '../../core/util/EventManager';
-import AppEvent from '../../core/util/AppEvent';
-import SiteProvider from '../common/SiteProvider';
-import AppManager from '../../core/util/AppManager';
+import CookieLaw from "vue-cookie-law";
+import AccountActionProvider from "../account/AccountActionProvider";
+import AccountService from "../../core/service/AccountService";
+import Message from "../common/Message";
+import EventManager from "../../core/util/EventManager";
+import AppEvent from "../../core/util/AppEvent";
+import SiteProvider from "../common/SiteProvider";
+import AppManager from "../../core/util/AppManager";
 
 export default {
-  name: 'Login',
-  mixins: [ AccountActionProvider, SiteProvider ],
+  name: "Login",
+  mixins: [AccountActionProvider, SiteProvider],
   components: {
-    CookieLaw, Message
+    CookieLaw,
+    Message,
   },
-  data () {
+  data() {
     return {
-      user: { },
-      username: '',
-      password: '',
+      user: {},
+      username: "",
+      password: "",
       posting: false,
-      showLegacyAccountLink: false
+      showLegacyAccountLink: false,
     };
   },
-  mounted () {
+  mounted() {
     let componentSelf = this;
     AccountService.hello()
       .then(function (userObj) {
@@ -79,19 +104,23 @@ export default {
       AccountService.login(userName, password, AppManager.uuid)
         .then(function (userObj) {
           componentSelf.user = userObj;
-          EventManager.publishAppEvent(AppEvent.ofInformation(AppEvent.ACCOUNT_LOGIN_SUCCESS));
+          EventManager.publishAppEvent(
+            AppEvent.ofInformation(AppEvent.ACCOUNT_LOGIN_SUCCESS)
+          );
           componentSelf.goToLastVisit();
         })
         .catch(function (error) {
           console.log(error);
-          EventManager.publishAppEvent(AppEvent.ofInformation(AppEvent.ACCOUNT_LOGIN_FAILURE, error));
+          EventManager.publishAppEvent(
+            AppEvent.ofInformation(AppEvent.ACCOUNT_LOGIN_FAILURE, error)
+          );
           componentSelf.posting = false;
           componentSelf.showLegacyAccountLink = true;
         });
     },
     cancel: function () {
       this.$router.back();
-    }
-  }
+    },
+  },
 };
 </script>
--- src/components/layout/TopNavigation.vue
+++ src/components/layout/TopNavigation.vue
@@ -2,90 +2,125 @@
   <b-navbar toggleable="md" fixed="top" type="dark" variant="dark">
     <b-navbar-toggle target="topnav_collapse"></b-navbar-toggle>
     <b-navbar-brand href="https://nexuspad.com/about.html" v-if="!isLoggedIn">
-      <img style="width:26px;" :class="{imageRotateHorizontal:loadingIcon}" src="https://nexuspad.com/images/np-logo.png"/>
+      <!-- <img
+          style="width: 26px"
+          :class="{ imageRotateHorizontal: loadingIcon }"
+          src="https://nexuspad.com/images/np-logo.png"
+        /> -->
+      <span
+        style="
+          margin-left: 1em;
+          font-family: Times New Roman, Times, Baskerville, Georgia, serif;
+          font-size: 1.4em;
+        "
+        >NexusPad</span
+      >
     </b-navbar-brand>
     <b-navbar-brand href="/" v-if="isLoggedIn && !onEdge">
-      <img style="width:26px;" :class="{imageRotateHorizontal:loadingIcon}" src="https://nexuspad.com/images/np-logo.png"/>
+      <img
+        style="width: 26px"
+        :class="{ imageRotateHorizontal: loadingIcon }"
+        src="https://nexuspad.com/images/np-logo.png"
+      />
     </b-navbar-brand>
     <b-navbar-brand v-if="isLoggedIn && onEdge">
       <router-link to="/activities">
-        <img style="width:26px;" :class="{imageRotateHorizontal:loadingIcon}" src="https://nexuspad.com/images/np-logo.png"/>
+        <img
+          style="width: 26px"
+          :class="{ imageRotateHorizontal: loadingIcon }"
+          src="https://nexuspad.com/images/np-logo.png"
+        />
       </router-link>
     </b-navbar-brand>
 
     <b-collapse is-nav id="topnav_collapse" v-if="isLoggedIn === false">
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/login">{{npContent('log in')}}</b-nav-item>
+        <b-nav-item to="/login">{{ npContent("log in") }}</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
 
     <b-collapse is-nav id="topnav_collapse" v-if="isLoggedIn === true">
       <b-navbar-nav>
-        <b-nav-item v-for="m in availableModules" :to="m.link" v-bind:key="m.id" :class="{'highlight' : activeModule == m.id}"
-          :active="activeModule == m.id">
-          {{npContent('m' + m.id)}}
+        <b-nav-item
+          v-for="m in availableModules"
+          :to="m.link"
+          v-bind:key="m.id"
+          :class="{ highlight: activeModule == m.id }"
+          :active="activeModule == m.id"
+        >
+          {{ npContent("m" + m.id) }}
         </b-nav-item>
         <b-nav-item></b-nav-item>
         <b-nav-form @submit="search">
-          <input ref="searchInput" class="form-control mr-sm-2" type="search" v-model="searchKeyword"
-            v-on:keyup.enter="search($event)" v-on:keyup.delete="clearSearch($event)" />
-          <b-button class="my-2 my-sm-0" v-on:click="search($event)">{{npContent('search')}}</b-button>
+          <input
+            ref="searchInput"
+            class="form-control mr-sm-2"
+            type="search"
+            v-model="searchKeyword"
+            v-on:keyup.enter="search($event)"
+            v-on:keyup.delete="clearSearch($event)"
+          />
+          <b-button class="my-2 my-sm-0" v-on:click="search($event)">{{
+            npContent("search")
+          }}</b-button>
         </b-nav-form>
       </b-navbar-nav>
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-item to="/account">{{npContent('settings')}}</b-nav-item>
-        <b-nav-item @click="logout()">{{npContent('log_out')}}</b-nav-item>
+        <b-nav-item to="/account">{{ npContent("settings") }}</b-nav-item>
+        <b-nav-item @click="logout()">{{ npContent("log_out") }}</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-import NPModule from '../../core/datamodel/NPModule';
-import EventManager from '../../core/util/EventManager';
-import AppEvent from '../../core/util/AppEvent';
-import NPError from '../../core/datamodel/NPError';
-import AppRoute from '../AppRoute';
-import SiteProvider from '../common/SiteProvider';
-import AccountActionProvider from '../account/AccountActionProvider';
-import AccountService from '../../core/service/AccountService';
-import PreferenceService from '../../core/service/PreferenceService';
+import NPModule from "../../core/datamodel/NPModule";
+import EventManager from "../../core/util/EventManager";
+import AppEvent from "../../core/util/AppEvent";
+import NPError from "../../core/datamodel/NPError";
+import AppRoute from "../AppRoute";
+import SiteProvider from "../common/SiteProvider";
+import AccountActionProvider from "../account/AccountActionProvider";
+import AccountService from "../../core/service/AccountService";
+import PreferenceService from "../../core/service/PreferenceService";
 
 export default {
-  name: 'TopNavigation',
-  mixins: [ SiteProvider, AccountActionProvider ],
-  data () {
+  name: "TopNavigation",
+  mixins: [SiteProvider, AccountActionProvider],
+  data() {
     return {
       activeModule: 0,
       availableModules: [],
-      searchKeyword: '',
+      searchKeyword: "",
       isLoggedIn: AccountService.isLoggedIn(),
-      loadingIcon: false
+      loadingIcon: false,
     };
   },
-  beforeCreate () {
-  },
-  mounted () {
+  beforeCreate() {},
+  mounted() {
     this.setModules();
 
-    if (this.$route && this.$route.query['keyword']) {
-      this.searchKeyword = this.$route.query['keyword'];
+    if (this.$route && this.$route.query["keyword"]) {
+      this.searchKeyword = this.$route.query["keyword"];
     } else {
-      this.searchKeyword = '';
+      this.searchKeyword = "";
     }
     EventManager.subscribe(AppEvent.LOADING, this.showLoadingIcon);
     EventManager.subscribe(AppEvent.ACCOUNT_LOGIN_SUCCESS, this.setModules);
-    EventManager.subscribe(AppEvent.ACCOUNT_MODULE_SETTINGS_UPDATE, this.setModules);
+    EventManager.subscribe(
+      AppEvent.ACCOUNT_MODULE_SETTINGS_UPDATE,
+      this.setModules
+    );
 
     let componentSelf = this;
     if (this.$refs.searchInput) {
-      this.$refs.searchInput.addEventListener('search', function() {
-        console.log('search value', this.value)
+      this.$refs.searchInput.addEventListener("search", function () {
+        console.log("search value", this.value);
         if (!this.value || this.value.length === 0) {
-          componentSelf.searchKeyword = '';
+          componentSelf.searchKeyword = "";
           componentSelf.clearSearch();
         }
       });
@@ -94,12 +129,12 @@ export default {
     this.setActiveModule();
     this.isEdgeApi();
   },
-  beforeDestroy () {
+  beforeDestroy() {
     EventManager.unSubscribe(AppEvent.LOADING, this.showLoadingIcon);
     EventManager.unSubscribe(AppEvent.ACCOUNT_LOGIN_SUCCESS, this.setModules);
   },
   methods: {
-    setModules () {
+    setModules() {
       let componentSelf = this;
       AccountService.hello()
         .then((userObj) => {
@@ -109,25 +144,21 @@ export default {
           }
           let moduleSetting = userObj.preference.moduleSettings;
           if (!moduleSetting || moduleSetting.length === 0) {
-            NPModule.ALL_MODULES.forEach(mid => {
-              componentSelf.availableModules.push(
-                {
-                  id: mid,
-                  name: NPModule.codeForId(mid),
-                  link: '/organize/' + NPModule.codeForId(mid)
-                }
-              );
-            })
+            NPModule.ALL_MODULES.forEach((mid) => {
+              componentSelf.availableModules.push({
+                id: mid,
+                name: NPModule.codeForId(mid),
+                link: "/organize/" + NPModule.codeForId(mid),
+              });
+            });
           } else {
             for (var name in moduleSetting) {
               if (moduleSetting[name] !== false) {
-                componentSelf.availableModules.push(
-                  {
-                    id: NPModule.idForCode(name),
-                    name: name,
-                    link: '/organize/' + name
-                  }
-                );
+                componentSelf.availableModules.push({
+                  id: NPModule.idForCode(name),
+                  name: name,
+                  link: "/organize/" + name,
+                });
               }
             }
           }
@@ -135,14 +166,17 @@ export default {
         .catch(function (error) {
           console.error(error);
           if (error instanceof NPError) {
-            if (error.errorCode === 'NO_SESSION' && componentSelf.$router.currentRoute.meta.requiresAuth === true) {
-              console.log('TopNavigation: redirect to login page');
-              componentSelf.$router.push({name: 'login'});
+            if (
+              error.errorCode === "NO_SESSION" &&
+              componentSelf.$router.currentRoute.meta.requiresAuth === true
+            ) {
+              console.log("TopNavigation: redirect to login page");
+              componentSelf.$router.push({ name: "login" });
             }
           }
         });
     },
-    setActiveModule () {
+    setActiveModule() {
       let module = AppRoute.module(this.$route);
 
       if (this.activeModule !== module) {
@@ -153,51 +187,54 @@ export default {
         }
       }
     },
-    isAvailable (moduleId) {
+    isAvailable(moduleId) {
       if (this.availableModules.indexOf(moduleId) !== -1) {
         return true;
       }
       return false;
     },
-    search (event) {
+    search(event) {
       if (event) {
         event.preventDefault();
       }
       let moduleId = AppRoute.module(this.$route);
-      let routeName = '';
+      let routeName = "";
       switch (moduleId) {
         case NPModule.CONTACT:
-          routeName = 'contactSearch';
+          routeName = "contactSearch";
           break;
         case NPModule.CALENDAR:
-          routeName = 'eventSearch';
+          routeName = "eventSearch";
           break;
         case NPModule.DOC:
-          routeName = 'docSearch';
+          routeName = "docSearch";
           break;
         case NPModule.BOOKMARK:
-          routeName = 'bookmarkSearch';
+          routeName = "bookmarkSearch";
           break;
         case NPModule.PHOTO:
-          routeName = 'photoSearch';
+          routeName = "photoSearch";
           break;
       }
-      this.$router.push({name: routeName, query: {keyword: this.searchKeyword}});
+      this.$router.push({
+        name: routeName,
+        query: { keyword: this.searchKeyword },
+      });
     },
-    clearSearch () {
-      if (this.searchKeyword == '') {
+    clearSearch() {
+      if (this.searchKeyword == "") {
         this.$router.push(AppRoute.moduleHome(this.$route));
       }
     },
-    accountPage () {
-      this.$router.push({name: 'account'});
+    accountPage() {
+      this.$router.push({ name: "account" });
     },
-    showLoadingIcon (loading) {
+    showLoadingIcon(loading) {
       this.loadingIcon = loading;
-    }
+    },
   },
   watch: {
-    '$route.path': function () {
+    "$route.path": function () {
       let componentSelf = this;
       AccountService.hello()
         .then(() => {
@@ -210,22 +247,24 @@ export default {
 
       this.setActiveModule();
     },
-    '$route.query.keyword': function (value) {
+    "$route.query.keyword": function (value) {
       if (!value) {
-        this.searchKeyword = '';
+        this.searchKeyword = "";
       } else {
         this.searchKeyword = value;
       }
     },
-    'searchKeyword': function (value) {
+    searchKeyword: function (value) {
       if (!value || value.trim().length === 0) {
         // this.$router.push(AppRoute.moduleHome(this.$route));
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-.highlight { color: #fff; }
+.highlight {
+  color: #fff;
+}
 </style>
